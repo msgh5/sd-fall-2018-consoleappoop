d946da5135d767229cdfd1364f9570cae496199f
Guilherme Guizado <guilherme.guizado@mitt.ca>
2019-02-11 09:48:17 -0600

    First app commit

---
+++ CSharpOOP/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharpOOP
+{
+    //Person class
+    public class Person
+    {
+        //Properties
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        //Constructor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        //Constructor
+        public Person(string name)
+        {
+            Name = name;
+            Age = 18;
+        }
+
+        //Method
+        public void ChangePersonalData(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new Exception("Name needs to be specified");
+            }
+
+            Name = name;
+        }
+
+        //Method Overload
+        public void ChangePersonalData(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ CSharpOOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a new instance (object) of the Person class
+            var personGui = new Person("Guilherme", 30);
+
+            //Calling a method
+            personGui.ChangePersonalData("Gui");
+
+            //Printing the information to the console
+            Console.WriteLine(personGui.Name);
+            Console.WriteLine(personGui.Age);
+
+            Console.ReadLine();
+        }
+    }
+}
